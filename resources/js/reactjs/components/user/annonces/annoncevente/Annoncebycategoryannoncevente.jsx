f99451060a4b5c2bea9c35dd64817fa7d0dccaaa
Boclair Temgoua <temgoua2013@gamil.com>
2020-05-05 15:57:30 +0200

    .

--- resources/js/reactjs/components/user/annonces/annoncevente/Annoncebycategoryannoncevente.jsx
+++ resources/js/reactjs/components/user/annonces/annoncevente/Annoncebycategoryannoncevente.jsx
@@ -369,7 +369,7 @@ class Annoncebycategoryannoncevente extends Component {
                                     <div className="col-lg-4 col-md-12 mx-auto">
 
                                         <div className="submit text-center">
-                                            <NavLink className="btn btn-danger" to={`/annonce/show/create/`}>
+                                            <NavLink className="btn btn-danger" to={`/annonce_vente/${this.props.match.params.annoncetype}/new/`}>
                                                 <i className="now-ui-icons ui-1_simple-add"/> <b>Poster votre annonce</b>
                                             </NavLink>
                                         </div>
