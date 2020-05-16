f38f50860d86b15795f40295aeed17b9adf759d6
Boclair Temgoua <temgoua2013@gamil.com>
2020-05-16 03:52:19 +0200

    fix seed

--- database/seeds/AnnonceblogSeeder.php
+++ database/seeds/AnnonceblogSeeder.php
@@ -16,9 +16,9 @@ class AnnonceblogSeeder extends Seeder
      */
     public function run()
     {
-        factory(blogannoncereservation::class, 3000)->create();
-        factory(blogannoncelocation::class, 3000)->create();
-        factory(blogannoncevente::class, 3000)->create();
+        factory(blogannoncereservation::class, 2000)->create();
+        factory(blogannoncelocation::class, 2000)->create();
+        factory(blogannoncevente::class, 2000)->create();
         factory(avisuser::class, 100)->create();
         factory(workwithus::class, 30)->create();
     }
