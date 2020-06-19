0c65ed833e4d1d052898ab91b4511e67dd1c2e79
Boclair Temgoua <temgoua2013@gamil.com>
2020-06-19 11:34:42 +0200

    .

--- resources/views/layouts/admin/_script.blade.php
+++ resources/views/layouts/admin/_script.blade.php
@@ -1,4 +1,16 @@
 @routes
+<script>
+    Number.prototype.formatMoney = function(decPlaces, thouSeparator, decSeparator) {
+        var n = this,
+            decPlaces = isNaN(decPlaces = Math.abs(decPlaces)) ? 2 : decPlaces,
+            decSeparator = decSeparator == undefined ? "." : decSeparator,
+            thouSeparator = thouSeparator == undefined ? "," : thouSeparator,
+            sign = n < 0 ? "-" : "",
+            i = parseInt(n = Math.abs(+n || 0).toFixed(decPlaces)) + "",
+            j = (j = i.length) > 3 ? j % 3 : 0;
+        return sign + (j ? i.substr(0, j) + thouSeparator : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thouSeparator) + (decPlaces ? decSeparator + Math.abs(n - i).toFixed(decPlaces).slice(2) : "");
+    };
+</script>
 <script src="/assets/dashboard/assets/js/core/jquery.min.js"></script>
 <script src="/assets/dashboard/assets/js/core/popper.min.js"></script>
 <script src="/assets/dashboard/assets/js/core/bootstrap-material-design.min.js"></script>
