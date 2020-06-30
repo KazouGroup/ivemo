79b5c41aa305084e4cabf62cff478d5e4579880f
Boclair Temgoua <temgoua2013@gamil.com>
2020-06-30 11:48:09 +0200

    fix mini bug

--- app/Notifications/ContactuserNotification.php
+++ app/Notifications/ContactuserNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ContactuserNotification extends Notification
+class ContactuserNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
--- app/Notifications/NewcommentNotification.php
+++ app/Notifications/NewcommentNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewcommentNotification extends Notification
+class NewcommentNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
