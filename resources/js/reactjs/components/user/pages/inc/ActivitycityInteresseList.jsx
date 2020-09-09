95e27e4c713b7420e1ffe2f92256989390576e45
Boclair Temgoua <temgoua2013@gamil.com>
2020-09-09 19:33:54 +0200

    fix bug

--- resources/js/reactjs/components/user/pages/inc/ActivitycityInteresseList.jsx
+++ resources/js/reactjs/components/user/pages/inc/ActivitycityInteresseList.jsx
@@ -44,8 +44,9 @@ class ActivitycityInteresseList extends PureComponent {
                                     </Link>
                                 </h6>
                             </div>
-                            <span
-                                dangerouslySetInnerHTML={{__html: (this.props.description.length > 96 ? this.props.description.substring(0, 96) + "<a class='text-dark' href=" + route('activitycity_show_site',[this.props.city.slug,this.props.slug]) + ">...<b>lire plus</b></a>" : this.props.description)}}/>
+                            <Link className="card-link" to={`/city/${this.props.city.slug}/a/${this.props.slug}/`}>
+                                <span dangerouslySetInnerHTML={{__html: (this.props.description.length > 96 ? this.props.description.substring(0, 96) + "..." : this.props.description)}}/>
+                            </Link>
 
 
                             <ButonFavorisLikedForInteressActivity {...this.props} unfavoriteItem={this.props.unfavoriteItem}
