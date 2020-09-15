02bfbdf6946716cc4434d3e43e505abd421e8f00
Boclair Temgoua <temgoua2013@gamil.com>
2020-09-15 19:38:38 +0200

    fix bug

--- database/migrations/2020_09_15_185536_add_data_to_annonce_table.php
+++ database/migrations/2020_09_15_185536_add_data_to_annonce_table.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class AddInfoTable extends Migration
+class AddDataToAnnonceTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,64 +13,38 @@ class AddInfoTable extends Migration
      */
     public function up()
     {
-        Schema::table('activitycities', function (Blueprint $table) {
-            $table->text('link_video')->after('title')->nullable();
-            $table->timestamp('expired_at')->default(now()->addDays(7))->after('created_at');
-        });
 
         Schema::table('annoncelocations', function (Blueprint $table) {
+
+
             $table->boolean('furniture')->after('status')->default(false)->nullable();
             $table->boolean('terrace')->after('status')->default(false)->nullable();
             $table->boolean('balcony')->after('status')->default(false)->nullable();
             $table->boolean('elevator')->after('status')->default(false)->nullable();
-            $table->string('phone_seller')->after('link_video')->nullable();
             $table->unsignedBigInteger('terrace_number')->after('link_video')->nullable();
             $table->unsignedBigInteger('balcony_number')->after('link_video')->nullable();
-            $table->string('contact_seller')->after('link_video')->nullable();
-            $table->text('link_video')->after('title')->nullable();
-            $table->unsignedBigInteger('periodeannonce_id')->after('user_id')->nullable()->index();
-            $table->timestamp('expired_at')->default(now()->addDays(7))->after('created_at');
         });
 
         Schema::table('annoncereservations', function (Blueprint $table) {
+
+
             $table->boolean('furniture')->after('status')->default(false)->nullable();
             $table->boolean('terrace')->after('status')->default(false)->nullable();
             $table->boolean('balcony')->after('status')->default(false)->nullable();
             $table->boolean('elevator')->after('status')->default(false)->nullable();
-            $table->string('phone_seller')->after('link_video')->nullable();
             $table->unsignedBigInteger('terrace_number')->after('link_video')->nullable();
             $table->unsignedBigInteger('balcony_number')->after('link_video')->nullable();
-            $table->string('contact_seller')->after('link_video')->nullable();
-            $table->text('link_video')->after('title')->nullable();
-            $table->timestamp('expired_at')->default(now()->addDays(7))->after('created_at');
         });
 
         Schema::table('annonceventes', function (Blueprint $table) {
+
             $table->boolean('furniture')->after('status')->default(false)->nullable();
             $table->boolean('terrace')->after('status')->default(false)->nullable();
             $table->boolean('balcony')->after('status')->default(false)->nullable();
             $table->boolean('elevator')->after('status')->default(false)->nullable();
-            $table->string('phone_seller')->after('link_video')->nullable();
             $table->unsignedBigInteger('terrace_number')->after('link_video')->nullable();
             $table->unsignedBigInteger('balcony_number')->after('link_video')->nullable();
-            $table->string('contact_seller')->after('link_video')->nullable();
-            $table->text('link_video')->after('title')->nullable();
-            $table->timestamp('expired_at')->default(now()->addDays(7))->after('created_at');
         });
-
-        Schema::table('forums', function (Blueprint $table) {
-            $table->timestamp('expired_at')->default(now()->addDays(7))->after('created_at');
-        });
-
-        Schema::table('employments', function (Blueprint $table) {
-            $table->text('link_video')->after('title')->nullable();
-            $table->timestamp('expired_at')->default(now()->addDays(7))->after('created_at');
-        });
-
-        $periodeannonces = array(
-            array('name' => 'le mois','slug' => str_slug('le mois'),'status' => true),
-        );
-        DB::table('periodeannonces')->insert($periodeannonces);
     }
 
     /**
@@ -81,9 +54,6 @@ class AddInfoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('activitycities');
-        Schema::dropIfExists('forums');
-        Schema::dropIfExists('employments');
         Schema::dropIfExists('annoncelocations');
         Schema::dropIfExists('annoncereservations');
         Schema::dropIfExists('annonceventes');
