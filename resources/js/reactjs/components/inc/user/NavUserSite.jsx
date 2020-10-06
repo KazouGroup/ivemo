7b7afd973f23ea3da0357201b7dbc172d9a65c0f
Boclair Temgoua <temgoua2013@gamil.com>
2020-10-06 22:11:10 +0200

    fix bug nav

--- resources/js/reactjs/components/inc/user/NavUserSite.jsx
+++ resources/js/reactjs/components/inc/user/NavUserSite.jsx
@@ -187,23 +187,44 @@ class NavUserSite extends PureComponent {
                                 </a>
                             </li>
                             :
-                            <li className="nav-item dropdown">
-                                <a href="#" className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown">
-                                    <i className="now-ui-icons ui-1_simple-add" aria-hidden="true"/>
-                                    <b>Poster votre annonce</b>
-                                </a>
-                                <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                                    <NavLink to={`/al_data/locations/new/`} className="dropdown-item">
-                                        <i className="now-ui-icons business_bank"/> Locations
-                                    </NavLink>
-                                    <NavLink to={`/av_data/ventes/new/`} className="dropdown-item">
-                                        <i className="now-ui-icons location_map-big"/> Ventes
-                                    </NavLink>
-                                    <NavLink to={`/employment/ab/new/`} className="dropdown-item">
-                                        <i className="now-ui-icons business_briefcase-24"/> Emplois & Services
-                                    </NavLink>
-                                </div>
-                            </li>
+                            <>
+
+                                {$userIvemo.email_verified_at ?
+                                    <>
+                                        {$userIvemo.status_profile ?
+                                            <li className="nav-item dropdown">
+                                                <a href="#" className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown">
+                                                    <i className="now-ui-icons ui-1_simple-add" aria-hidden="true"/>
+                                                    <b>Poster votre annonce</b>
+                                                </a>
+                                                <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                                                    <NavLink to={`/al_data/locations/new/`} className="dropdown-item">
+                                                        <i className="now-ui-icons business_bank"/> Locations
+                                                    </NavLink>
+                                                    <NavLink to={`/av_data/ventes/new/`} className="dropdown-item">
+                                                        <i className="now-ui-icons location_map-big"/> Ventes
+                                                    </NavLink>
+                                                    <NavLink to={`/employment/ab/new/`} className="dropdown-item">
+                                                        <i className="now-ui-icons business_briefcase-24"/> Emplois & Services
+                                                    </NavLink>
+                                                </div>
+                                            </li>
+                                            :
+                                            <li className="nav-item">
+                                                <a href={void (0)} style={{cursor :"pointer"}} className="nav-link" onClick={() => this.infoItem()}>
+                                                    <i className="now-ui-icons ui-1_simple-add"/> <b>Poster votre annonce</b>
+                                                </a>
+                                            </li>
+                                        }
+                                    </>
+                                    :
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link" to={`/email/verify/`}>
+                                            <i className="now-ui-icons ui-1_simple-add"/> <b>Poster votre annonce</b>
+                                        </NavLink>
+                                    </li>
+                                }
+                            </>
 
                         }
 
